adfb847841942d8ebeb1bd7d74129867e1c7d88a
openmask2025 <opunmasuku@gmail.com>
2025-05-14 05:07:40 +0900

    bbb

--- tes01.py
+++ tes01.py
@@ -1,5 +1,18 @@
 import pyxel
-pyxel.init(200, 150)
-pyxel.cls(8)
-pyxel.line(20, 20, 180, 130, 7)
-pyxel.show()
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120, title="Hello Pyxel")
+        pyxel.images[0].load(0, 0, "assets/pyxel_logo_38x16.png")
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
+        pyxel.blt(61, 66, 0, 0, 0, 38, 16)
+
+App()
